df5f0486d0febe3e65c18a8989ce8757da190c3f
byenio <marcel.bienko@gmail.com>
2024-01-12 14:13:06 +0100

    fix: remove unsafe code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,6 @@ export default async function Home() {
   const session = await getServerAuthSession();
   if (!session?.user) redirect("/login");
 
-  const admin = await api.role.create.mutate({ name: "ADMIN", level: 9 });
-  const moderator = await api.role.create.mutate({
-    name: "MODERATOR",
-    level: 6,
-  });
-  const tutor = await api.role.create.mutate({ name: "TUTOR", level: 3 });
-  const u = await api.role.create.mutate({ name: "USER", level: 0 });
-
   const user = await api.user.get.query();
   if (!user?.fullname) redirect("/personal-info");
   if (!user?.role) await api.user.setDefaultRole.query();

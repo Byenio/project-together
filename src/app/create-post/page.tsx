1e22d4a87aa0d6c53248f6a0ca41caa27ff57356
byenio <marcel.bienko@gmail.com>
2024-02-04 18:48:53 +0100

    feat: update create post form

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 import { api } from "~/trpc/server";
-import CreatePostForm from "./create-post-form";
+import CreatePostForm from "./form";
 
 export default async function CreatePost() {
   const { role } = (await api.user.getRole.query()) ?? {
@@ -15,9 +15,9 @@ export default async function CreatePost() {
   if (!canAccess) redirect("/");
 
   return (
-    <div className="m-auto max-w-[600px]">
+    <div className="m-auto max-w-[650px]">
       <div className="my-4 w-full text-center">
-        <h1 className="p-8 text-2xl text-neutral-content">Utwórz nowy post</h1>
+        <h1 className="text-neutral-content p-8 text-2xl">Utwórz nowy post</h1>
       </div>
       <CreatePostForm />
     </div>

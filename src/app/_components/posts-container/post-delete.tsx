83028fdf844d20239672fb69cf096a0b4bf67bca
byenio <marcel.bienko@gmail.com>
2024-02-22 19:49:22 +0100

    feat: confirmation modal for post delete

--- src/app/_components/posts-container/post-delete.tsx
+++ src/app/_components/posts-container/post-delete.tsx
@@ -1,12 +1,21 @@
 "use client";
 
-import { Button } from "@nextui-org/react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { api } from "~/trpc/react";
 import { DeleteIcon } from "../icons";
 
 export function PostDelete({ id }: { id: string }) {
   const router = useRouter();
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
 
   const postDelete = api.post.deleteById.useMutation({
     onSuccess: () => {
@@ -14,15 +23,46 @@ export function PostDelete({ id }: { id: string }) {
     },
   });
 
+  const handlePostDelete = () => {
+    postDelete.mutate({ postId: id });
+    onClose();
+  };
+
   return (
-    <Button
-      isIconOnly
-      color="danger"
-      size="sm"
-      variant="light"
-      onClick={() => postDelete.mutate({ postId: id })}
-    >
-      <DeleteIcon />
-    </Button>
+    <>
+      <Button
+        isIconOnly
+        color="danger"
+        size="sm"
+        variant="light"
+        onPress={onOpen}
+        // onClick={() => postDelete.mutate({ postId: id })}
+      >
+        <DeleteIcon />
+      </Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>Czy na pewno chcesz usunąć ten post?</ModalHeader>
+              <ModalBody>
+                <p className="text-default-600">
+                  Po usunięciu tego postu nie ma możliwości jego przywrócenia.
+                  Czy na pewno chcesz go usunąć?
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" variant="light" onPress={onClose}>
+                  Anuluj
+                </Button>
+                <Button color="danger" onPress={handlePostDelete}>
+                  Usuń post
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }

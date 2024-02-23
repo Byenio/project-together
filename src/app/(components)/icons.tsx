4cc92e1f8a2f83d1c4c9866c7f6ed940b586ee9e
byenio <marcel.bienko@gmail.com>
2024-02-23 16:44:21 +0100

    fix: change upvote and downvote icons

--- src/app/(components)/icons.tsx
+++ src/app/(components)/icons.tsx
@@ -340,12 +340,12 @@ export function UpvoteIcon() {
       stroke="currentColor"
       fill="currentColor"
       stroke-width="0"
-      viewBox="0 0 320 512"
-      height="1em"
-      width="1em"
+      viewBox="0 0 448 512"
+      height=".6em"
+      width=".6em"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"></path>
+      <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
     </svg>
   );
 }
@@ -356,12 +356,12 @@ export function DownvoteIcon() {
       stroke="currentColor"
       fill="currentColor"
       stroke-width="0"
-      viewBox="0 0 320 512"
-      height="1em"
-      width="1em"
+      viewBox="0 0 448 512"
+      height=".6em"
+      width=".6em"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
+      <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
     </svg>
   );
 }

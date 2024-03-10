0ce194861db4b9cf0827a66b748f5594abb52b7a
byenio <marcel.bienko@gmail.com>
2024-03-10 19:24:12 +0100

    Capacitor init

--- next.config.js
+++ next.config.js
@@ -10,3 +10,22 @@ module.exports = {
     ],
   },
 };
+
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "/avatars/**",
+      },
+    ],
+  },
+  output: "export",
+};
+
+module.exports = nextConfig;
